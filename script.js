0058435d91ac35eb363c6ed0a7de779a9f6b5a57
Sriramachandra <sriramachandra2387.k@gmail.com>
2022-07-03 21:12:21 +0530

    updated pic

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function animate() {
 animate();
 timelineRelativeTime();
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//     console.log('callback - particles.js config loaded');
+//   });
   
